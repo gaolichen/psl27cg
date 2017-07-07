587405825b56797487a4ed1d139763261718296e
Gaoli Chen <galic.chen@gmail.com>
2017-07-07 11:03:31 -0400

    tablize package file. do not automatically package from notebook

--- mpackage/extractGroupData.m
+++ mpackage/extractGroupData.m
@@ -1,30 +1,32 @@
-GAPToMathematica[pstr_] := StringReplace[pstr, {"[" -> "{", "]" -> "}", 
-      RegularExpression["E\\((\\d+)\\)"] -> "Exp[2*Pi*I/$1]"}]
- 
-ReadGAP[fname_] := Module[{f, str}, f = OpenRead[fname]; 
-      str = StringJoin[(StringJoin[GAPToMathematica[#1], "\n"] & ) /@ 
-         ReadList[f, String]]; Close[f]; Return[str]; ]
- 
-ReadGAPDataFromFile[fname_] := Module[{f}, 
-     f = StringToStream[ReadGAP[fname]]; Return[Read[f, Expression]]]
- 
-LoadGroupFromFile[filename_, opts:OptionsPattern[]] := 
-    Module[{f, ret, name, value}, f = StringToStream[ReadGAP[filename]]; 
-      While[True, name = Read[f, Expression]; If[name == EndOfFile, Break[]]; 
-        ret[name] = Read[f, Expression]; ]; Return[ret]; ]
-GAPToMathematica[pstr_] := StringReplace[pstr, {"[" -> "{", "]" -> "}", 
-      RegularExpression["E\\((\\d+)\\)"] -> "Exp[2*Pi*I/$1]"}]
- 
-ReadGAP[fname_] := Module[{f, str}, f = OpenRead[fname]; 
-      str = StringJoin[(StringJoin[GAPToMathematica[#1], "\n"] & ) /@ 
-         ReadList[f, String]]; Close[f]; Return[str]; ]
- 
-ReadGAPDataFromFile[fname_] := Module[{f}, 
-     f = StringToStream[ReadGAP[fname]]; Return[Read[f, Expression]]]
- 
-LoadGroupFromFile[filename_, opts:OptionsPattern[]] := 
-    Module[{f, ret, name, value, keys = {}}, 
-     f = StringToStream[ReadGAP[filename]]; While[True, 
-       name = Read[f, Expression]; If[name == EndOfFile, Break[]]; 
-        AppendTo[keys, name]; ret[name] = Read[f, Expression]; ]; 
-      ret["Keys"] = keys; Return[ret]; ]
+(* ::Package:: *)
+
+Clear[GAPToMathematica];
+GAPToMathematica[pstr_]:=StringReplace[pstr,{"["->"{","]"->"}",RegularExpression["E\\((\\d+)\\)"]->"Exp[2*Pi*I/$1]" }];
+
+Clear[ReadGAP];
+ReadGAP[fname_]:=Module[{f,str},
+	f=OpenRead[fname];
+	str=StringJoin[Map[StringJoin[GAPToMathematica[#],"\n"]&,ReadList[f,String]]];
+	Close[f];
+	Return[str];
+];
+
+ClearAll[ReadGAPDataFromFile];
+ReadGAPDataFromFile[fname_]:=Module[{f},
+	f=StringToStream[ReadGAP[fname]];
+	Return[Read[f, Expression]]
+];
+
+ClearAll[LoadGroupFromFile];
+LoadGroupFromFile[filename_, opts:OptionsPattern[]]:=Module[{f, ret, name, value,keys={}},
+	f=StringToStream[ReadGAP[filename]];
+	While[True, 
+		name = Read[f, Expression];
+		If[name == EndOfFile, Break[]];
+		AppendTo[keys,name];
+		ret[name]=Read[f,Expression];
+	];
+
+	ret["Keys"]=keys;
+	Return[ret];
+];
--- mpackage/psl27-common.m
+++ mpackage/psl27-common.m
@@ -1,38 +1,73 @@
-DecomposePoly[poly_, basis_, opts:OptionsPattern[]] := 
-    Module[{ret, i, j, vars, terms, rep, pp, coef, term, phases, 
-      invertedPhases}, ret = ConstantArray[0, Length[basis]]; 
-      pp = Expand[poly]; vars = OptionValue[Vars]; 
-      phases = OptionValue[Phases]; invertedPhases = 
-       Table[phases[[i]] -> 1/phases[[i]], {i, 1, Length[phases]}]; 
-      If[Length[vars] == 0, vars = Variables[poly]]; 
-      rep = Table[vars[[i]] -> 1, {i, 1, Length[vars]}]; 
-      For[i = 1, i <= Length[basis], i++, terms = MonomialList[basis[[i]]]; 
-        For[j = 1, j <= Length[terms], j++, coef = terms[[j]] /. rep; 
-          term = Simplify[terms[[j]]/coef]; ret[[i]] += Coefficient[pp, term]*
-            ComplexExpand[Conjugate[coef /. invertedPhases]]; ]; 
-        ret[[i]] = Simplify[ret[[i]]]; ]; Return[Simplify[ret]]]
+(* ::Package:: *)
+
+(* common functions for psl27-g4 CG coefficients calculation. *)
+ClearAll[DecomposePoly];
+Options[DecomposePoly]={Vars->{},Phases->{}};
+DecomposePoly[poly_,basis_,opts:OptionsPattern[]]:=
+	Module[{ret,i,j,vars,terms,rep,pp,coef, term,phases,invertedPhases},
+	ret = ConstantArray[0,Length[basis]];
+	pp=Expand[poly];
+	(*Print["pp=",pp];*)
+	vars = OptionValue[Vars];
+	phases=OptionValue[Phases];
+	invertedPhases=Table[phases[[i]]->1/phases[[i]],{i,1,Length[phases]}];
+	If[Length[vars]==0,vars=Variables[poly]];
+	rep=Table[vars[[i]]->1,{i,1,Length[vars]}];
+	For[i=1,i<= Length[basis],i++,
+		terms=MonomialList[basis[[i]]];
+		(*Print["terms=",terms];*)
+		For[j=1,j<= Length[terms],j++,
+		coef =terms[[j]]/.rep;
+		term = Simplify[terms[[j]]/coef];
+		(*Print["coef=",Coefficient[pp,term], "*",coef];*)
+		ret[[i]] += Coefficient[pp,term] * ComplexExpand[Conjugate[coef/.invertedPhases]];
+	];
+	ret[[i]]=Simplify[ret[[i]]];
+	(*Print["ret=",ret[[i]]];*)
+	];
+
+	Return[Simplify[ret]]
+];
+
  
-Options[DecomposePoly] = {Vars -> {}, Phases -> {}}
+ClearAll[DecomposePolyMat];
+Options[DecomposePolyMat]=Join[{},Options[DecomposePoly]];
+DecomposePolyMat[mat_,basis_,opts:OptionsPattern[]]:=Table[
+	DecomposePoly[mat[[i]], basis,FilterRules[{opts},Options[DecomposePoly]]],
+	{i,1,Length[mat]}
+];
+
+TestConjugateTransform[op_,mat_]:=Module[{opc},
+	opc=Simplify[mat.Conjugate[op].Inverse[mat]];
+	Return[Simplify[opc-op]]
+];
  
-DecomposePolyMat[mat_, basis_, opts:OptionsPattern[]] := 
-    Table[DecomposePoly[mat[[i]], basis, FilterRules[{opts}, 
-       Options[DecomposePoly]]], {i, 1, Length[mat]}]
- 
-Options[DecomposePolyMat] = {Vars -> {}, Phases -> {}}
- 
-TestConjugateTransform[op_, mat_] := Module[{opc}, 
-     opc = Simplify[mat . Conjugate[op] . Inverse[mat]]; 
-      Return[Simplify[opc - op]]]
- 
-VerifyPsl27Generator[A_, B_] := Module[{AB, n, zero, ABc}, 
-     n = Length[A]; If[n != Length[B], Return[False]]; 
-      zero = ConstantArray[0, {n, n}]; AB = N[A . B]; 
-      ABc = N[A . B . A . B . B]; If[(Chop[N[A . A] - IdentityMatrix[n]] === 
-         zero) == False, Return[False]]; 
-      If[(Chop[N[MatrixPower[B, 3]] - IdentityMatrix[n]] === zero) == False, 
-       Print["VerifyPsl27Generator failed: B^3 != I."]; Return[False]]; 
-      If[(Chop[N[MatrixPower[AB, 7]] - IdentityMatrix[n]] === zero) == False, 
-       Print["VerifyPsl27Generator failed: (AB)^7 != I."]; Return[False]]; 
-      If[(Chop[N[MatrixPower[ABc, 4]] - IdentityMatrix[n]] === zero) == 
-        False, Print["VerifyPsl27Generator failed: [A,B]^4 != I."]; 
-        Return[False]]; Return[True]; ]
+(* Verify Psl27 Generators.*)
+VerifyPsl27Generator[A_,B_]:=Module[{AB,n,zero,ABc},
+	n=Length[A];
+	If[n!= Length[B],Return[False]];
+	zero = ConstantArray[0,{n,n}];
+
+	AB=N[A.B];
+	ABc=N[A.B.A.B.B];
+	If[SameQ[Chop[N[A.A]-IdentityMatrix[n]],zero]==False,
+		Return[False]
+	];
+
+	If[SameQ[Chop[N[MatrixPower[B,3]]-IdentityMatrix[n]],zero]==False,
+		Print["VerifyPsl27Generator failed: B^3 != I."];
+		Return[False]
+	];
+
+	If[SameQ[Chop[N[MatrixPower[AB,7]]-IdentityMatrix[n]],zero]==False,
+		Print["VerifyPsl27Generator failed: (AB)^7 != I."];
+	Return[False]
+	];
+
+	If[SameQ[Chop[N[MatrixPower[ABc,4]]-IdentityMatrix[n]],zero]==False,
+		Print["VerifyPsl27Generator failed: [A,B]^4 != I."];
+		Return[False]
+	];
+
+	Return[True];
+];
